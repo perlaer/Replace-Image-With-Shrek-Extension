1471f0ca598bbca8734cacfad876c5d3d3a377ec
perlaer <gabrielcilliansidhom@gmail.com>
2025-12-30 18:48:20 +0000

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,16 @@
+NEW_IMAGE_URL = "https://static.wikia.nocookie.net/universalstudios/images/f/f2/Shrek2-disneyscreencaps.com-4369.jpg/revision/latest/scale-to-width-down/1000?cb=20250224023204";
 
+function replaceImages() {
+  document.querySelectorAll("img").forEach(img => {
+    img.src = NEW_IMAGE_URL;
+    img.srcset = "";
+  });
+}
+
+replaceImages()
+
+const observer = new MutationObserver(replaceImages);
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
